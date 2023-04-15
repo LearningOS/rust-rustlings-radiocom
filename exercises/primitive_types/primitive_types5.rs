14d24259ed88b19944e81dda8d8ce63553341d02
Kyle <radiocom@qq.com>
2023-04-15 14:33:50 +0800

    commit and switch to remote ssh machine.

--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -2,11 +2,15 @@
 // Destructure the `cat` tuple so that the println will work.
 // Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
+
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+    let age  = cat;
+
+    // make a pattern that matches the tuple
+    let (name, age) = cat;
+
 
     println!("{} is {} years old.", name, age);
 }
